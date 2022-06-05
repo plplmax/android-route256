40821c90ded6dcc0887a9ac9affcbcb029e4344e
plplmax <953-plplmax@users.noreply.gitlab.ozon.dev>
2022-06-05 19:31:57 +0300

    Revert "copy a project from workshop"
    This reverts commit 668b56fdaa725b2347c8dd388b87ad37999212ac.

--- app/src/main/kotlin/dev/ozon/gitlab/plplmax/homework2/domain/products/repository/ProductsRepository.kt
+++
@@ -1,8 +0,0 @@
-package dev.ozon.gitlab.plplmax.homework2.domain.products.repository
-
-import dev.ozon.gitlab.plplmax.homework2.presentation.products.ProductUi
-
-interface ProductsRepository {
-    fun getProducts(): List<ProductUi>
-    fun getProductById(guid: String): ProductUi?
-}
