4447224181315b66ea95a0a48bec0f39f9733513
plplmax <953-plplmax@users.noreply.gitlab.ozon.dev>
2022-06-07 07:26:05 +0300

    refactor: move MockProductsRepository to domain

--- app/src/main/kotlin/dev/ozon/gitlab/plplmax/homework2/di/ServiceLocator.kt
+++ app/src/main/kotlin/dev/ozon/gitlab/plplmax/homework2/di/ServiceLocator.kt
@@ -1,7 +1,7 @@
 package dev.ozon.gitlab.plplmax.homework2.di
 
-import dev.ozon.gitlab.plplmax.homework2.data.products.repository.MockProductsRepositoryImpl
 import dev.ozon.gitlab.plplmax.homework2.domain.products.interactor.ProductsInteractor
+import dev.ozon.gitlab.plplmax.homework2.domain.products.repository.ProductsRepository
 
 // Подробнее можете почитать [тут](http://sergeyteplyakov.blogspot.com/2013/03/di-service-locator.html),
  // [тут](https://habr.com/ru/post/465395/) и [тут](https://javatutor.net/articles/j2ee-pattern-service-locator).
@@ -9,7 +9,7 @@ import dev.ozon.gitlab.plplmax.homework2.domain.products.interactor.ProductsInte
 object ServiceLocator {
     val productsInteractor: ProductsInteractor by lazy {
         ProductsInteractor.Base(
-            MockProductsRepositoryImpl()
+            ProductsRepository.Mock()
         )
     }
 }
--- app/src/main/kotlin/dev/ozon/gitlab/plplmax/homework2/domain/products/repository/ProductsRepository.kt
+++ app/src/main/kotlin/dev/ozon/gitlab/plplmax/homework2/domain/products/repository/ProductsRepository.kt
@@ -1,8 +1,19 @@
 package dev.ozon.gitlab.plplmax.homework2.domain.products.repository
 
+import dev.ozon.gitlab.plplmax.homework2.data.products.ProductData
+import dev.ozon.gitlab.plplmax.homework2.data.products.productsData
+import dev.ozon.gitlab.plplmax.homework2.domain.products.mapper.toUi
 import dev.ozon.gitlab.plplmax.homework2.presentation.products.ProductUi
 
 interface ProductsRepository {
     fun getProducts(): List<ProductUi>
     fun getProductById(guid: String): ProductUi?
+
+    class Mock : ProductsRepository {
+        override fun getProducts(): List<ProductUi> = productsData.map(ProductData::toUi)
+
+        override fun getProductById(guid: String): ProductUi? { // TODO: Remove nullability in the next homework
+            return productsData.find { it.guid == guid }?.toUi()
+        }
+    }
 }
