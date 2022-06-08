3ab1cf105f8b3366308381c707353bcfc7c95ee3
plplmax <953-plplmax@users.noreply.gitlab.ozon.dev>
2022-06-08 17:33:17 +0300

    feat: create ProductInDetailData

---
+++ app/src/main/kotlin/dev/ozon/gitlab/plplmax/homework2/data/products/ProductInDetailData.kt
@@ -0,0 +1,16 @@
+package dev.ozon.gitlab.plplmax.homework2.data.products
+
+data class ProductInDetailData(
+    val guid: String,
+    val name: String,
+    val price: String,
+    val description: String,
+    val rating: Double,
+    val isFavorite: Boolean,
+    val isInCart: Boolean,
+    val images: List<String>,
+    val weight: Double?,
+    val count: Int?,
+    val availableCount: Int?,
+    val additionalParams: Map<String, String>
+)
