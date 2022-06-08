faa9a8b8f72edf2998ac58e6e100c97b253e8276
plplmax <953-plplmax@users.noreply.gitlab.ozon.dev>
2022-06-08 17:36:05 +0300

    feat: add mapping to ProductInDetailUi

--- app/src/main/kotlin/dev/ozon/gitlab/plplmax/homework2/domain/products/mapper/ProductMapper.kt
+++ app/src/main/kotlin/dev/ozon/gitlab/plplmax/homework2/domain/products/mapper/ProductMapper.kt
@@ -1,7 +1,24 @@
 package dev.ozon.gitlab.plplmax.homework2.domain.products.mapper
 
 import dev.ozon.gitlab.plplmax.homework2.data.products.ProductData
+import dev.ozon.gitlab.plplmax.homework2.data.products.ProductInDetailData
 import dev.ozon.gitlab.plplmax.homework2.presentation.products.ProductUi
+import dev.ozon.gitlab.plplmax.homework2.presentation.products.productInDetail.ProductInDetailUi
 
 fun ProductData.toUi(): ProductUi =
     ProductUi(guid, image, name, price, rating, isFavorite, isInCart)
+
+fun ProductInDetailData.toUi(): ProductInDetailUi = ProductInDetailUi(
+    guid,
+    name,
+    price,
+    description,
+    rating,
+    isFavorite,
+    isInCart,
+    images,
+    weight,
+    count,
+    availableCount,
+    additionalParams
+)
