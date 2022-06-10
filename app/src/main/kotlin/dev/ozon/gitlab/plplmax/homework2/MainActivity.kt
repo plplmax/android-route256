7bcfde063e1ecfe6cd3fa0dde670961271f5fcbc
plplmax <953-plplmax@users.noreply.gitlab.ozon.dev>
2022-06-10 11:12:16 +0300

    refactor: replace fragmentManager with NavController

--- app/src/main/kotlin/dev/ozon/gitlab/plplmax/homework2/MainActivity.kt
+++ app/src/main/kotlin/dev/ozon/gitlab/plplmax/homework2/MainActivity.kt
@@ -2,18 +2,33 @@ package dev.ozon.gitlab.plplmax.homework2
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import dev.ozon.gitlab.plplmax.homework2.presentation.products.productInDetail.ProductInDetailFragment
 
-class MainActivity : AppCompatActivity(), ProductInDetailFragment.ToProductDetailNavigationListener {
+class MainActivity : AppCompatActivity(),
+    ProductInDetailFragment.ToProductDetailNavigationListener {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = findNavController()
     }
 
     override fun navigateToDetailFragment(productGuid: String) {
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragmentContainer, ProductInDetailFragment.newInstance(productGuid))
-            .addToBackStack(null)
-            .commit()
+        navController.navigate(
+            R.id.action_productsFragment_to_productInDetailFragment,
+            ProductInDetailFragment.newBundle(productGuid)
+        )
+    }
+
+    private fun findNavController(): NavController {
+        return supportFragmentManager.findFragmentById(R.id.fragmentContainer).let {
+            val navHostFragment = it as NavHostFragment
+            navHostFragment.navController
+        }
     }
 }
